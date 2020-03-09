8eb1ca38230c1750003e73258947aa9aadd6886c
André Reis <42789233+andrereis99@users.noreply.github.com>
2020-03-09 19:11:21 +0000

    Add files via upload

--- venv/solver.py
+++ venv/solver.py
@@ -1,13 +1,86 @@
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,7,9]]
+testBoard = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
 
-import numpy as np
+def possibleLine(board, num, pos):
+        for i in range(len(board[0])):
+                if board[pos[0]][i] == num and pos[1] != i:
+                        return False
+        return True
 
-print(np.matrix(grid))
+def possibleColumn(board, num, pos):
+        for i in range(len(board)):
+                if board[i][pos[1]] == num and pos[0] != i:
+                        return False
+        return True
+
+def possibleSquare(board, num, pos):
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y * 3, box_y * 3 + 3):
+                for j in range(box_x * 3, box_x * 3 + 3):
+                        if board[i][j] == num and (i, j) != pos:
+                                return False
+
+        return True
+
+def possible(board, num, pos):
+    if possibleLine(board, num, pos) and possibleColumn(board, num, pos) and possibleSquare(board, num, pos):
+            return True
+    return False
+
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def findEmptySpace(board):
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == 0:
+                return (row, col)
+
+    return None
+
+def solve(board):
+    find = findEmptySpace(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if possible(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+print_board(testBoard)
+print("___________________")
+solve(testBoard)
+print_board(testBoard)
